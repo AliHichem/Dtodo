b23472f8997606348e7e9629b0190c32d917d683
hichem ali <ali.hichem@mail.com>
2023-07-29 18:43:45 +0100

    small solidity opimization for less gass use

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,38 +3,40 @@ pragma solidity ^0.8.19;
 
 contract TodoList {
 
-    uint public taskCount = 0;
+    uint8 public taskCount = 0;
 
     struct Task {
-        uint id;
+        uint8 id;
         string content;
         bool completed;
     }
 
     event TaskCreated(
-        uint id,
+        uint8 id,
         string content,
         bool completed
     );
 
     event TaskCompleted(
-        uint id,
+        uint8 id,
         bool completed
     );
 
-    mapping(uint => Task) public tasks;
+    mapping(uint8 => Task) public tasks;
 
     constructor() {
         createTask("New task created.");
     }
 
     function createTask(string memory _content) public {
-        taskCount ++;
+        unchecked {
+            taskCount ++;
+        }
         tasks[taskCount] = Task(taskCount, _content, false);
         emit TaskCreated(taskCount, _content, false);
     }
 
-    function toggleCompleted(uint _id) public {
+    function toggleCompleted(uint8 _id) public {
         Task memory _task = tasks[_id];
         _task.completed = !_task.completed;
         tasks[_id] = _task;
